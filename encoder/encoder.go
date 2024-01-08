c6003719e6737cd61993b4bdb65dcb8463e30392
aixoio <79969530+aixoio@users.noreply.github.com>
2024-01-08 20:56:02 +1100

    Add basic file reader to encoder

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -1,5 +1,24 @@
 package encoder
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/aixoio/fsend/packets"
+	"github.com/fatih/color"
+)
+
 func Start(filename string) {
+	filedata, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println(color.RedString("%s: %s", err.Error(), filename))
+		return
+	}
+
+	packet_data := packets.FileData_Packet{
+		Name: filename,
+		Data: filedata,
+	}
+
 	
 }
